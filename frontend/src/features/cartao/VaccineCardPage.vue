0089709101e9995d3a55a929acadde4a9784031c
LUIS_NOTEBOOK <luishfmendes226@gmail.com>
2026-09-16 19:39:59 -0300

    feat(frontend:vaccineCardPage): monta página do cartão de vacina com header e lista Adiciona VaccineList ao VaccineCardPage e encapsula os componentes em div page para centralizar o layout verticalmente.

--- frontend/src/features/cartao/VaccineCardPage.vue
+++ frontend/src/features/cartao/VaccineCardPage.vue
@@ -1,11 +1,25 @@
 <script setup>
 import HeaderInfo from './HeaderInfo.vue'
+import VaccineList from './VaccineList.vue';
 </script>
 
 <!-- HTML -->
 <template>
-  <HeaderInfo></HeaderInfo>
+  <div class="page">
+    <HeaderInfo></HeaderInfo>
+    <VaccineList></VaccineList>
+  </div>
 </template>
 
 <!-- CSS -->
-<style scoped></style>
+<style scoped>
+.page{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  gap: 15px;
+}
+</style>
