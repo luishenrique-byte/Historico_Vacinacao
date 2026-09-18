ba5ba6ff54e98ed62d0b551f30e827ad0d4e7f11
LUIS_NOTEBOOK <luishfmendes226@gmail.com>
2026-09-18 17:49:10 -0300

    feat(frontend:vaccineCardPage): adiciona botões de voltar e imprimir Adiciona BackButton com tema azul e PrintButton à página. Inclui @media print para ajustar padding e alinhamento do layout na impressão.

--- frontend/src/features/cartao/VaccineCardPage.vue
+++ frontend/src/features/cartao/VaccineCardPage.vue
@@ -1,5 +1,7 @@
 <script setup>
+import BackButton from '@/shared/components/BackButton.vue';
 import HeaderInfo from './HeaderInfo.vue'
+import PrintButton from './PrintButton.vue';
 import VaccineList from './VaccineList.vue';
 </script>
 
@@ -9,11 +11,14 @@ import VaccineList from './VaccineList.vue';
     <HeaderInfo></HeaderInfo>
     <VaccineList></VaccineList>
   </div>
+  <PrintButton></PrintButton>
+  <BackButton :contraste="true"></BackButton>
 </template>
 
 <!-- CSS -->
 <style scoped>
 .page{
+  position: relative;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -21,5 +26,12 @@ import VaccineList from './VaccineList.vue';
   width: 100%;
   height: 100%;
   gap: 15px;
+}
+/* Vaccine Card Page */
+@media print{
+  .page{
+    padding: 30px 0px 0px 0px;
+    justify-content: flex-start;
+  }
 }
 </style>
