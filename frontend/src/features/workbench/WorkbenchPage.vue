39574e118b83a72ea3884b789c07e60a7216aadb
LUIS_NOTEBOOK <luishfmendes226@gmail.com>
2026-09-22 11:19:12 -0300

    feat(frontend:workbenchPage): monta página do workbench com header e menu
    Adiciona HeaderInfo, Workbench e BackButton ao WorkbenchPage, seguindo o mesmo padrão de layout da VaccineCardPage.

--- frontend/src/features/workbench/WorkbenchPage.vue
+++ frontend/src/features/workbench/WorkbenchPage.vue
@@ -1,13 +1,25 @@
-<script setup></script>
-<template>
-    <div>
+<script setup>
+import BackButton from '@/shared/components/BackButton.vue';
+import HeaderInfo from '../../shared/components/HeaderInfo.vue';
+import Workbench from './component/Workbench.vue';
 
+</script>
+<template>
+    <div class="page">
+        <HeaderInfo></HeaderInfo>
+        <Workbench></Workbench>
     </div>
+    <BackButton :contraste="true"></BackButton>
 </template>
 <style scoped>
-div {
-    height: 50px;
-    width: 50px;
-    background-color: red;
+.page{
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  gap: 15px;
 }
 </style>
