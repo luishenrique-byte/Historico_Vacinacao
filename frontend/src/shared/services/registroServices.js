296e1cd7568b2823bb8a94b486858bc9998f5830
LUIS_NOTEBOOK <luishfmendes226@gmail.com>
2026-09-22 21:34:50 -0300

    feat(frontend:registroServices): adiciona método de criação de registro
    Adiciona função save que realiza POST em /v1/registro
    para criar um novo registro de vacinação.

--- frontend/src/shared/services/registroServices.js
+++ frontend/src/shared/services/registroServices.js
@@ -5,3 +5,7 @@ const BASE_URL = 'http://localhost:8082/v1/registro'
 export function findAllByPaciente(id){
     return axios.get(BASE_URL + '/paciente/'+id)
 }
+
+export function save(registro){
+    return axios.post(BASE_URL,registro)
+}
